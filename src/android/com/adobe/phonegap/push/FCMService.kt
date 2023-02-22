cb28bbe75201b408dedcc8116e362823d0f178ff
randomdev3994 <111250354+randomdev3994@users.noreply.github.com>
2023-02-22 09:45:34 +0000

    Update FCMService.kt

--- src/android/com/adobe/phonegap/push/FCMService.kt
+++ src/android/com/adobe/phonegap/push/FCMService.kt
@@ -45,6 +45,20 @@ class FCMService : FirebaseMessagingService() {
 
     private val messageMap = HashMap<Int, ArrayList<String?>>()
 
+    private val FLAG_MUTABLE = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {≤
+      PendingIntent.FLAG_MUTABLE
+    }
+    else {
+      0
+    }
+    
+    private val FLAG_IMMUTABLE = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {≤
+      PendingIntent.FLAG_IMMUTABLE
+    }
+    else {
+      0
+    }
+    
     /**
      * Get the Application Name from Label
      */
@@ -448,7 +462,7 @@ class FCMService : FirebaseMessagingService() {
       this,
       requestCode,
       notificationIntent,
-      PendingIntent.FLAG_UPDATE_CURRENT
+      PendingIntent.FLAG_UPDATE_CURRENT or FLAG_IMMUTABLE
     )
     val dismissedNotificationIntent = Intent(
       this,
@@ -467,7 +481,7 @@ class FCMService : FirebaseMessagingService() {
       this,
       requestCode,
       dismissedNotificationIntent,
-      PendingIntent.FLAG_CANCEL_CURRENT
+      PendingIntent.FLAG_CANCEL_CURRENT or FLAG_IMMUTABLE
     )
 
     val mBuilder: NotificationCompat.Builder =
@@ -676,7 +690,7 @@ class FCMService : FirebaseMessagingService() {
                   this,
                   uniquePendingIntentRequestCode,
                   intent,
-                  PendingIntent.FLAG_ONE_SHOT
+                  PendingIntent.FLAG_ONE_SHOT or FLAG_MUTABLE
                 )
               } else {
                 Log.d(TAG, "push receiver for notId $notId")
@@ -685,7 +699,7 @@ class FCMService : FirebaseMessagingService() {
                   this,
                   uniquePendingIntentRequestCode,
                   intent,
-                  PendingIntent.FLAG_ONE_SHOT
+                  PendingIntent.FLAG_ONE_SHOT or FLAG_MUTABLE
                 )
               }
             }
@@ -696,7 +710,7 @@ class FCMService : FirebaseMessagingService() {
               pIntent = PendingIntent.getActivity(
                 this, uniquePendingIntentRequestCode,
                 intent,
-                PendingIntent.FLAG_UPDATE_CURRENT
+                PendingIntent.FLAG_UPDATE_CURRENT or FLAG_IMMUTABLE
               )
             }
 
@@ -706,7 +720,7 @@ class FCMService : FirebaseMessagingService() {
               pIntent = PendingIntent.getBroadcast(
                 this, uniquePendingIntentRequestCode,
                 intent,
-                PendingIntent.FLAG_UPDATE_CURRENT
+                PendingIntent.FLAG_UPDATE_CURRENT or FLAG_IMMUTABLE
               )
             }
           }
